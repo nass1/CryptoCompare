39f7e525ce7c1f7e826e1fe52cd8e7d065d07a27
Nass <n.nass-1@hotmail.com>
2018-01-19 08:04:03 +0200

    made it readable
    we added readable next will make price more readable and work on the search

--- CoinCompare/views.py
+++ CoinCompare/views.py
@@ -5,6 +5,17 @@ from django.shortcuts import render
 from django.views import View
 import requests
 import json
+import math
+
+millnames = ['',' Thousand',' Million',' Billion',' Trillion']
+
+def millify(n):
+    n = float(n)
+    millidx = max(0,min(len(millnames)-1,
+                        int(math.floor(0 if n == 0 else math.log10(abs(n))/3))))
+
+    return '{:.0f}{}'.format(n / 10**(3 * millidx), millnames[millidx])
+
 
 class HomePage(TemplateView):
     template_name = "index.html"
@@ -13,21 +24,49 @@ class HomePage(TemplateView):
 #next is convert numbers into readable stuff
 def index(request):
 
-    url = "https://api.coinmarketcap.com/v1/ticker/?convert=AUD&limit=10"
+    url = "https://api.coinmarketcap.com/v1/ticker/?convert=AUD&limit=20"
     respo =  requests.get(url)
     print (respo)
     data = json.loads(respo.content.decode('utf-8'))
+    print ("let wjhat data type is? ",type(data[0]))
+
+
+    count = 0
+    print ("value is: ", type(data[1]["max_supply"]))
+
+    for i in data:
+
+        numAvailable = i["available_supply"]
+        totalSupply = i["total_supply"]
+        maxSupply = i["max_supply"]
+        lastHours = i["24h_volume_aud"]
+        marketCap = i["market_cap_aud"]
+
+
+        numAvailable
+        data[count]["numAvailable"] = millify(float(numAvailable))
+        data[count]["numAvailableReadale"] = "{:,}".format(float(numAvailable))
+
+        data[count]["totalSupply"] = millify(float(totalSupply))
+        data[count]["totalSupplyReadale"] = "{:,}".format(float(totalSupply))
+        if maxSupply == None:
+            data[count]["maxSupply"] = "Null"
+            data[count]["maxSupplyReadale"] ="Null"
+        else:
+            data[count]["maxSupply"] = millify(float(maxSupply))
+            data[count]["maxSupplyReadale"] = "{:,}".format(float(maxSupply))
+
+        data[count]["lastHours"] = millify(float(lastHours))
+        data[count]["lastHoursReadale"] = "{:,}".format(float(lastHours))
+
+        data[count]["marketCap"] = millify(float(marketCap))
+        data[count]["marketCapReadale"] = "{:,}".format(float(marketCap))
+
+        count +=1
 
     with open('JSONdata.JSON', 'w') as f:
         json.dump(data, f, ensure_ascii=False)
     f.close()
-    datalist = []
-    '''
-    for i in data:
-        datalist.append(i["name"])
-        datalist.append(i["symbol"])
-        datalist.append("_______________")
-    '''
     context = {'data': data}
     return render(request, 'index.html', context)
 
